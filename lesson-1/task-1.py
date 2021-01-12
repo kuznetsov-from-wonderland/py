3432ddfe08c5404716219a4cb6acb495ed08b07d
Ilya Kuznetsov <kromsik@icloud.com>
2021-01-12 20:32:01 +0300

    Task 1 ✅

---
+++ lesson-1/task-1.py
@@ -0,0 +1,7 @@
+first_name = 'Ilya'
+last_name = 'Kuznetsov'
+isDeveloper = True
+years_of_experience = 7
+
+buddy = input('What is your name? ')
+print(f"Hello, {buddy}. I'm {first_name} {last_name}. Nice to meet.")

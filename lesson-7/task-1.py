94ac4de2d64e4927b643bcc7eb27c52e1b4e0e87
Ilya Kuznetsov <kromsik@icloud.com>
2021-02-03 21:32:49 +0300

    Task 1 __add__ ✅

--- lesson-7/task-1.py
+++ lesson-7/task-1.py
@@ -20,11 +20,27 @@
 class Matrix:
     def __init__(self, values):
         self.values = values
+        self.dimentions = (len(values), len(values[0]))
 
     def __str__(self):
         return str('\n'.join(['\t'.join(list(map(str, i))) for i in self.values]))
 
+    def __add__(self, other):
+        result = self.values
+        for i in range(self.dimentions[0]):
+            for j in range(other.dimentions[1]):
+                result[i][j] = self.values[i][j] + other.values[i][j]
+        return Matrix(result)
+
 
 mtrx = Matrix([[1, 1, 0], [2, 17, 113], [50, 14, -8]])
-
 print(mtrx)
+print()
+m1 = Matrix([[1, 1, 0]])
+m2 = Matrix([[2, 17, 113]])
+print(m1 + m2)
+print()
+
+m3 = Matrix([[1, 1, 0], [2, 17, 113], [50, 14, -8]])
+m4 = Matrix([[-1, -1, 0], [2, 17, 113], [-50, -14, 8]])
+print(m3 + m4)
